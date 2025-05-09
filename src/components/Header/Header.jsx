70dc9ba7d94d9627d92bcfe54fe84a9d48b451ff
Alok4D <alokroy602701@gmail.com>
2025-05-09 20:56:22 +0600

    completed the header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,53 @@
-import './Header.css'
+import "./Header.css";
+import HeaderTitle from "../../ui/HeaderTitle";
+import { profile1 } from "../../assets";
+import CodeBlock from "../../ui/CodeBlock/CodeBlock";
+import Facts from "../../ui/Facts/Facts";
+import SocialHandles from "../../ui/SocialHandles/SocialHandles";
+import BreathCircle from "../../ui/BreathCircle"
 
+const code = `
+const developer = {
+    firstName: "Alok",
+    lastName: "Roy"
+    aka: "DJ",
+    hobby: repeat = () => {
+        //coder()
+        //developer()
+        //code()
+        //repeat();
+        }
+    }
+`
 const Header = () => {
-    return (
-        <div>
-            header
+  return (
+    <header id="header">
+        <BreathCircle/>
+      <div className="section__wrapper">
+        <div className="grid upper">
+          <div className="column">
+            <HeaderTitle />
+          </div>
+          <div className="column">
+            <img src={profile1} className="profile__photo" id="banner-photo" alt="" />
+          </div>
         </div>
-    );
+        <div className="card grid lower">
+          <CodeBlock
+            language={"javascript"}
+            code={code}
+          />
+          <div>
+            <p className="text__muted description">
+            I dissect intricate user experience challenges to engineer integrity-focused solutions that resonate with billions of users.
+            </p>
+            <Facts></Facts>
+            <SocialHandles></SocialHandles>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/ui/CodeBlock/CodeBlock.jsx
@@ -0,0 +1,39 @@
+import "./CodeBlock.css";
+import PropTypes from "prop-types";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import js from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
+import { a11yLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
+SyntaxHighlighter.registerLanguage("javascript", js);
+
+const CodeBlock = ({ language, code }) => {
+  return (
+    <div className="code__Block">
+      <div className="flex__center dot__container">
+        <span className="dot"></span>
+        <span className="dot"></span>
+        <span className="dot"></span>
+      </div>
+      <SyntaxHighlighter
+        language={language}
+        style={a11yLight}
+        customStyle={{
+          background: "rgba(var(--color-primary-rgb), 0.03)",
+          color: "skyblue",
+          padding: "20px",
+          fontSize: "14px",
+        }}
+        wrapLongLines={true}
+      >
+        {code}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
+
+// ✅ Correct PropTypes
+CodeBlock.propTypes = {
+  code: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+};
+
+export default CodeBlock;
