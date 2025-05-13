d590f37633c25dd188132e2caa4511796decfa56
Alok4D <alokroy602701@gmail.com>
2025-05-13 12:32:49 +0600

    skill section design complete

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,10 +1,24 @@
+import { skills } from '../../data';
+import SkillCard from '../../ui/SkillCard/SkillCard';
 import './Skill.css';
 
 const Skill = () => {
     return (
-        <div>
-            skill
-        </div>
+        <section id='skills'>
+            <div className='section__wrapper'>
+                <div className='section__header'>
+                    <h2 className='shine'>My Skills</h2>
+                </div>
+                <div className='skill__container'>
+                    {skills.map((list, index) => (
+                        <SkillCard 
+                        {...list}
+                        key={index}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 };
 
